a86be30fc9fcbd54c7734507d351d51944828d1a
theblockhouse <scottmckay87@gmail.com>
2018-10-29 18:20:18 -0600

    added spawn wait time to heart item

--- scriptable_objects/powerups/Health.gd
+++ scriptable_objects/powerups/Health.gd
@@ -1,21 +1,27 @@
 extends KinematicBody2D
 
+const WAIT_TO_SPAWN = 0.6
 const MOVE_SPEED = 3
 const MOVE_DIST_Y = 40
 const MIN_MOVE_DIST = 1
 const STAY_TIME = 1
 const HEALTH = 50
 
+var ready = false
 var spawned = false
 var target_loc = Vector2()
 var intended_player = null
 
 func _ready():
+	hide()
 	target_loc = global_position
 	target_loc.y -= MOVE_DIST_Y
+	yield(get_tree().create_timer(WAIT_TO_SPAWN), 'timeout')
+	show()
+	ready = true
 
 func _physics_process(delta):
-	if !spawned:
+	if !spawned or !ready:
 		return
 	
 	var dist_to_target = global_position.distance_to(target_loc)
