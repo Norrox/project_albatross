69dd39961d54be8326b1562ad4295a7b8fea0b2b
theblockhouse <scottmckay87@gmail.com>
2018-11-01 03:10:39 -0600

    tweaked shell casing values

--- weapons/bullet/Shell.gd
+++ weapons/bullet/Shell.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 
 const SHOW_TIME = 2
 const MIN_MOVE_DIST = 1
-const MAX_MOVE_DIST = 100
-const EXTRA_FORCE_Y = -50
+const MAX_MOVE_DIST = 60
+const EXTRA_FORCE_Y = -25
 const MAX_ROTATE_SPEED = 5
 
 var target_pos = Vector2()
@@ -15,7 +15,7 @@ func _ready():
 	rotate_dir = randi()%1-1
 	rotate_speed = randi()%MAX_ROTATE_SPEED-MAX_ROTATE_SPEED
 	target_pos = global_position
-	target_pos += Vector2(randi()%MAX_MOVE_DIST-MAX_MOVE_DIST, randi()%MAX_MOVE_DIST-MAX_MOVE_DIST)
+	target_pos -= Vector2(randi()%MAX_MOVE_DIST-MAX_MOVE_DIST, randi()%-MAX_MOVE_DIST-MAX_MOVE_DIST)
 	target_pos.y += EXTRA_FORCE_Y
 	$AnimationPlayer.play('idle')
 	yield(get_tree().create_timer(SHOW_TIME),'timeout')
@@ -23,7 +23,7 @@ func _ready():
 	
 func _physics_process(delta):
 	if global_position.distance_to(target_pos) > MIN_MOVE_DIST:
-		move_and_slide(target_pos - global_position)
+		move_and_slide((target_pos - global_position) * 5) 
 		rotation += rotate_dir * delta * rotate_speed
 	else:
 		$AnimationPlayer.play('on_ground')
