cfcc7903fa4d31f9120de4877e9eb2941502729c
theblockhouse <scottmckay87@gmail.com>
2018-11-16 20:00:55 -0700

    fixed layers

--- Settings.gd
+++ Settings.gd
@@ -12,6 +12,6 @@ enum MATCH_TYPE {
 	ffa_8
 	}
 
-var player_type = PLAYER_TYPE.skeleton
+var player_type = PLAYER_TYPE.human
 var enable_ultra_graphics = false
 var match_type = MATCH_TYPE.ffa_8
--- interface/Menu.gd
+++ interface/Menu.gd
@@ -5,8 +5,8 @@ var button_busy = false
 func _ready():
 	if OS.get_name() == 'Windows':
 		Network.ready = true
-		#Global.goto_scene('res://Game.tscn')
-		Global.goto_scene('res://GrassLevel.tscn')
+		Global.goto_scene('res://Game.tscn')
+		#Global.goto_scene('res://GrassLevel.tscn')
 		#Global.goto_scene('res://SpaceLevel.tscn')
 	else:
 		Settings.player_type = Settings.PLAYER_TYPE.skeleton
